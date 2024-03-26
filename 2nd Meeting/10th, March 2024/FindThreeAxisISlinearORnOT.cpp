e6df6270140277cf906fe9dedc273ff512a55c5e
DearPratipal <kumarpratipal123@gmail.com>
2024-03-27 01:20:01 +0530

    add missing return

---
+++ 2nd Meeting/10th, March 2024/FindThreeAxisISlinearORnOT.cpp
@@ -0,0 +1,2 @@
+//Given three points (x1, y1), (x2, y2) and (x3, y3), write a program to check if all the three points fall on one straight line. 
+
---
+++ 2nd Meeting/10th, March 2024/GivenPointToFindOrigin.cpp
@@ -0,0 +1,11 @@
+//Given a point (x, y), write a program to find out if it lies on the x-axis, y-axis or at the origin, viz. (0, 0).
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+
+return ;
+}
