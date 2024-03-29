930cb7e32ce4b32490d5131ca8d79834faff758a
DearPratipal <kumarpratipal123@gmail.com>
2024-03-29 10:48:04 +0530

    OutputisRunning

---
+++ 3rd Meeting/14th, March 2024/PridictOutput4.cpp
@@ -0,0 +1,28 @@
+/* Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main( ) {
+int t = 10;
+while (t /= 2) {
+cout << "Hello" << endl;
+}
+}
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+/* int t = 10; */
+
+int t; //update here to n number 
+
+cout<<"Enter The OWN Number : ";
+cin>>t;
+
+while (t /= 2) {
+cout << "Hello" << endl;
+}
+}
