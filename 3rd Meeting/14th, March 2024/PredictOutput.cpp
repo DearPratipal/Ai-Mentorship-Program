0e3123c360cdd46ce5ac9d1f317eb9c5188e0dee
DearPratipal <kumarpratipal123@gmail.com>
2024-03-29 10:37:09 +0530

    OutputPridication

---
+++ 3rd Meeting/14th, March 2024/PredictOutput.cpp
@@ -0,0 +1,25 @@
+/* PREDICT THE OUTPUT
+#include <iostream>
+using namespace std;
+
+int main() {
+    while ('a' < 'b')
+        cout << "malayalam is a palindrome" << endl;
+    return 0;
+} */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a=10;
+    int b=5;
+
+    while ('a' < 'b'){
+        cout << "malayalam is a palindrome" << endl;
+        //some changing 
+    }
+       
+    return 0;
+}
