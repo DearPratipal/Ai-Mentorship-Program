bdd5deb2103a22e40f038bb99113dc07d9359fb7
DearPratipal <kumarpratipal123@gmail.com>
2024-03-29 10:41:33 +0530

    Net Output

---
+++ 3rd Meeting/14th, March 2024/Prid3rdOutput.cpp
@@ -0,0 +1,20 @@
+/* Predict the output
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+while ('1' < '2')
+cout << "In while loop" << endl;
+}
+
+Predict the output
+ */
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    while ('1' < '2')
+    cout << "In while loop" << endl;
+}
+
+//loop not stoping
