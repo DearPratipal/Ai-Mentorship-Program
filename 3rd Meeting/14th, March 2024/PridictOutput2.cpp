fb606c7b03a8ba2ecaa57b17f55828d79622a167
DearPratipal <kumarpratipal123@gmail.com>
2024-03-29 10:39:40 +0530

    2nd Output

---
+++ 3rd Meeting/14th, March 2024/PridictOutput2.cpp
@@ -0,0 +1,25 @@
+/* PREDICT THE OUTPUT
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    while (i = 10) {
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+} */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i;
+    cin>>i;
+    while (i = 10) {
+        cout << i << endl;
+        i = i + 1;
+    }
+    return 0;
+}
