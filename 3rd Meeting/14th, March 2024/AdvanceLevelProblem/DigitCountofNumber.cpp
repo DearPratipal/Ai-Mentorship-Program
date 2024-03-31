13e7e36a0b5bab7c0cbb148969573b9142e112de
DearPratipal <kumarpratipal123@gmail.com>
2024-03-31 16:19:51 +0530

    Solve Add Code

--- 3rd Meeting/14th, March 2024/AdvanceLevelProblem/DigitCountofNumber.cpp
+++ 3rd Meeting/14th, March 2024/AdvanceLevelProblem/DigitCountofNumber.cpp
@@ -1,8 +1,22 @@
 //WAP to count digits of a given number.
 
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int main (){
-    
+int main()
+{
+    int num;
+    cout << "Enter the Digit Countable Number :";
+    cin >> num;
+    int DIgits = 0;
+    if (num == 0)
+    {
+        cout << "the no of digit is 1.";
+    }
+    for (int i = 1; num > 0; i++)
+    {
+        DIgits = DIgits + 1;
+        num = num / 10;
+    }
+    cout << "The Number is :" << DIgits;
+    return 0;
 }
