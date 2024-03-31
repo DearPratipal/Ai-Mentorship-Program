070e236ec0cce19f9721d3b83c426d079bc0f89f
DearPratipal <kumarpratipal123@gmail.com>
2024-04-01 00:43:29 +0530

    Pattern of Square Number

--- 4th Meeting/15th, March 2024/PatternPrintOFSOlidSquare.cpp
+++ 4th Meeting/15th, March 2024/PatternPrintOFSOlidSquare.cpp
@@ -7,3 +7,27 @@ Print the Solid Square  pattern
 
 */
 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Num. of Lines :";
+    cin>>n;
+    /* cout<<"Enter the Num. of Rows :";
+    cin>>m;
+ */
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+    
+}
--- 4th Meeting/15th, March 2024/PrintPatternOfSquareNumber.cpp
+++ 4th Meeting/15th, March 2024/PrintPatternOfSquareNumber.cpp
@@ -4,3 +4,28 @@
 1 2 3 4 
 1 2 3 4
  */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Num. of Lines :";
+    cin>>n;
+    /* cout<<"Enter the Num. of Rows :";
+    cin>>m;
+ */
+
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+
+            cout<<" "<<j ;
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+    
+}
