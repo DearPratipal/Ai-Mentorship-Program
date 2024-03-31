216c877ea1e5f039baf4dedbc5c4d3978b5eebd7
DearPratipal <kumarpratipal123@gmail.com>
2024-04-01 01:05:18 +0530

    Reverse Tringle

--- 4th Meeting/15th, March 2024/PrintReverseStarTringle.cpp
+++ 4th Meeting/15th, March 2024/PrintReverseStarTringle.cpp
@@ -5,4 +5,25 @@
 *
     */
 
-   
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the Num. of Lines :";
+    cin>>n;
+
+    for(int i=n; i>=1; i--){
+        for(int j=1; j<=i; j++){
+
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    
+
+    return 0;
+    
+}
