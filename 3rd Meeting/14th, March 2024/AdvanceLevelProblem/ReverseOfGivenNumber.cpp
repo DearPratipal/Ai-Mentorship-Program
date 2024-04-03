dd78ff795ea71fe55d12aa14678dc8120c53ea94
DearPratipal <kumarpratipal123@gmail.com>
2024-04-03 22:52:47 +0530

    Push

--- 3rd Meeting/14th, March 2024/AdvanceLevelProblem/ReverseOfGivenNumber.cpp
+++ 3rd Meeting/14th, March 2024/AdvanceLevelProblem/ReverseOfGivenNumber.cpp
@@ -1,2 +1,13 @@
 //WAP to print reverse of a given number. 
 
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Reverse of a given number : ";
+    cin>>n;
+
+    return 0;
+}
