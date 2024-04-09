9fba16c076d2f80a2568633e9d0e9fcfd8eefe43
DearPratipal <kumarpratipal123@gmail.com>
2024-04-09 15:28:57 +0530

    Complete Advance Level

--- 3rd Meeting/14th, March 2024/AdvanceLevelProblem/ReverseOfGivenNumber.cpp
+++ 3rd Meeting/14th, March 2024/AdvanceLevelProblem/ReverseOfGivenNumber.cpp
@@ -5,11 +5,21 @@ using namespace std;
 
 int main(){
 
-    int n;
+    int n, rvsnum, remainder;
     cout<<"Reverse of a given number : ";
     cin>>n;
 
-    
+    remainder=0;
 
+    for (int i = 1; n > 0; i++){
+
+        int remainder = n % 10;
+        n = n / 10;
+        rvsnum = (rvsnum * 10) + remainder;
+    }
+    cout << "the reverse of given number is:" << rvsnum << endl;
+    
     return 0;
 }
+
+
--- 3rd Meeting/14th, March 2024/AdvanceLevelProblem/SumOfNumberDigit.cpp
+++ 3rd Meeting/14th, March 2024/AdvanceLevelProblem/SumOfNumberDigit.cpp
@@ -5,7 +5,19 @@ using namespace std;
 
 int main (){
 
-    
+    int n, sumOfDigits;
+    cout << "Enter the Num Digit:";
+    cin >> n;
+
+    sumOfDigits = 0;
+
+    for (int i = 1; n > 0; i++)
+    {
+        int remainder = n % 10;
+        n = n / 10;
+        sumOfDigits = sumOfDigits + remainder;
+    }
+    cout << "The Sum of Num Digit:" << sumOfDigits << endl;
     
     return 0;
 }
