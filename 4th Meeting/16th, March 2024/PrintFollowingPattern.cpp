6fef0cce5791965bedd8dbc94558ac2b24fcb7c2
DearPratipal <kumarpratipal123@gmail.com>
2024-04-11 23:50:18 +0530

    Commit

---
+++ 4th Meeting/16th, March 2024/PrintFollowingPattern.cpp
@@ -0,0 +1,28 @@
+/* Print the following pattern
+Input: n = 4
+Output:
+A
+A B
+A B C
+A B C D */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    
+    cout << "Enter the value of n: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        char ch = 'A';
+        for (int j = 1; j <= i; ++j) {
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
---
+++ 4th Meeting/16th, March 2024/ReverseNumTringle.cpp
@@ -0,0 +1,27 @@
+/* Print the following pattern
+Input: n = 4
+Output:
+1 2 3 4
+1 2 3
+1 2
+1 */
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+    int rows;
+    cout << "Enter the number of rows: ";
+    cin >> rows;
+
+    
+    for (int i = rows; i >= 1; --i) {
+        
+        for (int j = 1; j <= i; ++j) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
