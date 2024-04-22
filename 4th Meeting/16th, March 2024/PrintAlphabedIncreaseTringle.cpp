ea6af457def797bdfc37e60e9e90723cb6e7a212
DearPratipal <kumarpratipal123@gmail.com>
2024-04-22 20:23:35 +0530

    Edit Patern Printing

--- 4th Meeting/16th, March 2024/PrintAlphabedIncreaseTringle.cpp
+++ 4th Meeting/16th, March 2024/PrintAlphabedIncreaseTringle.cpp
@@ -1,9 +1,9 @@
 /* Print the following pattern
 Input : n = 4
 Output :
- A
- A B
- A B C
+A
+A B
+A B C
 A B C D */
 
 #include <iostream>
--- 6th Meeting/23th, March 2024/ArrayStudentRoll_Mark.cpp
+++ 6th Meeting/23th, March 2024/ArrayStudentRoll_Mark.cpp
@@ -3,3 +3,12 @@ mark of any student is less than 35 print its roll
 number. [roll number here refers to the index of the
 array.] */
 
+#include<iostream>>
+using namespace std;
+
+int main(){
+
+    
+
+return 0;
+}
