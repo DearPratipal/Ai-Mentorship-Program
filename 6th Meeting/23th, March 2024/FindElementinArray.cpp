a6ae82f76f2715511a3389d785fae7da2a3e1578
DearPratipal <kumarpratipal123@gmail.com>
2024-04-24 22:37:24 +0530

    Array

--- 6th Meeting/23th, March 2024/FindElementinArray.cpp
+++ 6th Meeting/23th, March 2024/FindElementinArray.cpp
@@ -2,3 +2,29 @@
 x as input.
  */
 
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[5];
+    for (int idx = 0; idx < 5; idx++)
+    {
+        cin >> arr[idx];
+    }
+
+    int elementx;
+    cout << "enter the value of element x :";
+    cin >> elementx;
+
+    int result = 0;
+    for (int idx = 0; idx < 5; idx++)
+    {
+        if (arr[idx] == elementx)
+        {
+            cout << "the element " << elementx << " is present at index " << idx<<endl;
+        }
+        else
+        continue;
+    }
+    return 0;
+}
--- 6th Meeting/23th, March 2024/SumOfElementGivenArray.cpp
+++ 6th Meeting/23th, March 2024/SumOfElementGivenArray.cpp
@@ -1,3 +1,16 @@
 /* Calculate the sum of all the elements in the
 given array. */
 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {65,47,89,32,14};
+    int sum = 0;
+    for (int idx = 0; idx < 5; idx++)
+    {
+        sum = sum + arr[idx];
+        
+    }
+    cout<<"the sum of element of arry is:"<<sum;
+    return 0;
+}
