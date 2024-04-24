10d286a47eb5048509ff23cc5b9d71cf92d8fc45
DearPratipal <kumarpratipal123@gmail.com>
2024-04-24 23:21:08 +0530

    DubletArray

--- 7th Meeting/26th, March 2024/FindTheDoubletinArray.cpp
+++ 7th Meeting/26th, March 2024/FindTheDoubletinArray.cpp
@@ -1,3 +1,32 @@
 /* Find the doublet in the Array whose sum is 
 equal to the given value x (LEETCODE TWO SUM) */
 
+#include <iostream>
+using namespace std;
+
+
+int main (){
+
+int array[]={3,4,6,7,1};
+
+int targetsum;
+cout<<"Enter the Target Element: "<<endl;
+cin>>targetsum;
+
+
+int size=5;
+int pairs=0;
+
+for(int i=0;i<size;i++){7
+    for(int j=i+1;j<size; j++){
+    
+    if(array[i]+array[j]==targetsum){
+        pairs++;
+            }
+        }
+    }
+    cout<<pairs<<endl;
+
+
+return 0;
+}
