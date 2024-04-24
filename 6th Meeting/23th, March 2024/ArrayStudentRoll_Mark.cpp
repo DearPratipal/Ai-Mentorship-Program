2b6bcc6709c11c089d8406f6fdcf565f4bf898ce
DearPratipal <kumarpratipal123@gmail.com>
2024-04-24 22:40:20 +0530

    ArrayMaxValue

--- 6th Meeting/23th, March 2024/ArrayStudentRoll_Mark.cpp
+++ 6th Meeting/23th, March 2024/ArrayStudentRoll_Mark.cpp
@@ -3,12 +3,21 @@ mark of any student is less than 35 print its roll
 number. [roll number here refers to the index of the
 array.] */
 
-#include<iostream>>
+#include<iostream>
 using namespace std;
-
 int main(){
-
-    
-
-return 0;
+    int marks[5] = {65,47,89,32,14};
+    int rollNo = marks[0];
+    for (int idx = 0; idx < 5; idx++)
+    {
+        if (marks[idx] < 35)
+        {
+            rollNo = idx;
+            cout<<"the roll number of student having number less than 35 is :"<<rollNo<<endl; 
+        }
+        else
+        continue;
+        
+    }
+    return 0;
 }
--- 6th Meeting/23th, March 2024/FindtheMaximumValueinArray.cpp
+++ 6th Meeting/23th, March 2024/FindtheMaximumValueinArray.cpp
@@ -1,3 +1,21 @@
 /* Find the maximum value out of all the elements
 in the array. */
 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {65,47,89,32,14};
+    int maximumValue = arr[0]; 
+    for (int idx = 0; idx < 5; idx++)
+    {
+       if (maximumValue<arr[idx])
+       {
+        maximumValue = arr[idx];
+       }
+        else
+        continue; 
+        
+    }
+     cout<<"the maximum value out of array element is :"<<maximumValue;
+    return 0;
+}
