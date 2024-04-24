49971d2f7720569125b7977d908b995fb4811f71
DearPratipal <kumarpratipal123@gmail.com>
2024-04-24 22:42:49 +0530

    ProductofArray

--- 6th Meeting/24th, March 2024/CalculateTheProductofGivenArray.cpp
+++ 6th Meeting/24th, March 2024/CalculateTheProductofGivenArray.cpp
@@ -1,2 +1,15 @@
 // Calculate the product of all the elements in the given array.
 
+#include<iostream>
+using namespace std;
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    int product = 1;
+    for (int idx = 0; idx < 5; idx++)
+    {
+        product= product * arr[idx];
+        
+    }
+    cout<<"the product of element of arry is:"<<product;
+    return 0;
+}
