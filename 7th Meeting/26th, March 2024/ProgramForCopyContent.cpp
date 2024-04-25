f62b5a5b11e38bf1ba5a4be1bc4f7b8f33e408a7
DearPratipal <kumarpratipal123@gmail.com>
2024-04-25 15:53:19 +0530

    Rotate K step arrya

--- 7th Meeting/26th, March 2024/ProgramForCopyContent.cpp
+++ 7th Meeting/26th, March 2024/ProgramForCopyContent.cpp
@@ -1,3 +1,32 @@
 /* Write a program to copy the contents of one 
 array into another in the reverse order.  */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    const int size = 5; // Change the size as needed
+    int original[size] = {1, 2, 3, 4, 5};
+    int reversed[size];
+
+    
+    for (int i = 0; i < size; ++i) {
+        reversed[i] = original[size - 1 - i];
+    }
+
+    // Displaying the original array
+    cout << "Original array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << original[i] << " ";
+    }
+    cout << endl;
+
+    // Displaying the reversed array
+    cout << "Reversed array: ";
+    for (int i = 0; i < size; ++i) {
+        cout << reversed[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
--- 7th Meeting/26th, March 2024/RotateGivenArrayFromKStep.cpp
+++ 7th Meeting/26th, March 2024/RotateGivenArrayFromKStep.cpp
@@ -1,6 +1,47 @@
-/* Rotate the given array ‘a’ by k steps, where k is 
-non-negative.  */
+/* Rotate the given array ‘a’ by k steps, where k is non-negative.  */
+// Note : k can be greater than n as well where n is the size of array ‘a'
 
-// Note : k can be greater than n as well where n is the 
-// size of array ‘a'
+#include <iostream>
+using namespace std;
 
+void rotate(int arr[], int n, int k) {
+    int temp[k];
+
+        for (int i = 0; i < k; ++i) {
+            temp[i] = arr[i];
+        }
+
+        for (int i = k; i < n; ++i) {
+            arr[i - k] = arr[i];
+        }
+        
+        for (int i = 0; i < k; ++i) {
+            arr[n - k + i] = temp[i];
+        }
+}
+
+int main() {
+    // Example usage
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k; // Number of steps to rotate
+
+    cout<<"Enter the K - Step : ";
+    cin>>k;
+
+    cout << "Original array: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    rotate(arr, n, k);
+
+    cout << "Array after rotating by " << k << " steps: ";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
